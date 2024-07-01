41083a2518c86178b34a586a7032bf6a6bf35da3
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-01 18:49:44 +0530

    separated data file for common data

---
+++ src/components/data/index.ts
@@ -0,0 +1,56 @@
+export const skills = [
+  {
+    title: "HTML & CSS",
+    percentage: "55%",
+  },
+  {
+    title: "Javascript",
+    percentage: "70%",
+  },
+  {
+    title: "React Js",
+    percentage: "80%",
+  },
+  {
+    title: "Next Js",
+    percentage: "60%",
+  },
+];
+
+export const achievements = [
+  {
+    title: "2+",
+    disc: "Years of experience",
+  },
+  {
+    title: "2",
+    disc: "Certifications",
+  },
+  {
+    title: "10+",
+    disc: "Projects",
+  },
+];
+
+export const navbarMenu = [
+  {
+    title: "Home",
+    id: "#home",
+  },
+  {
+    title: "Skills",
+    id: "#skills",
+  },
+  {
+    title: "Experience",
+    id: "#experience",
+  },
+  {
+    title: "Project",
+    id: "#projects",
+  },
+  {
+    title: "Contact",
+    id: "#contact",
+  },
+];
