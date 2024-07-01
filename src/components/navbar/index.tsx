8ca96463ceb8b8153036ea73b769fbeecbf70147
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-01 18:50:22 +0530

    Code optimization by removing extra code

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,32 +1,33 @@
+import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { navbarMenu } from "../data";
 
 function Navbar() {
+  const [currentSection, setCurrentSection] = useState("Home");
+
   return (
-    <div className="my-5 mx-[10%] flex items-center justify-between">
-      <p className="text-2xl italic">Portfolio</p>
-      <ul className="flex gap-10 text-base list-none items-center">
-        <li>
-          <AnchorLink href="#home">Home</AnchorLink>
-        </li>
-        <li>
-          <AnchorLink href="#skills">Skills</AnchorLink>
-        </li>
-        <li>
-          <AnchorLink href="#experience">Experience</AnchorLink>
-        </li>
-        <li>
-          <AnchorLink href="#projects">Projects</AnchorLink>
-        </li>
-        <li>
-          <AnchorLink href="#contact">Contact</AnchorLink>
-        </li>
+    <nav className="my-5 mx-[10%] flex items-center justify-between">
+      <p className="text-2xl font-bold">Niraj</p>
+      <ul className="flex gap-10 text-base list-none items-center uppercase">
+        {navbarMenu.map((nav, index) => {
+          const { title, id } = nav;
+          return (
+            <li
+              key={index}
+              onClick={() => setCurrentSection(title)}
+              className={`${currentSection === title ? "underline" : ""}`}
+            >
+              <AnchorLink href={id}>{title}</AnchorLink>
+            </li>
+          );
+        })}
       </ul>
-      <div className="navbar-btn">
+      <div className="navbar-btn linear-gradient-bg zoom-effect">
         <AnchorLink href="#contact" className="">
           Contact With Me
         </AnchorLink>
       </div>
-    </div>
+    </nav>
   );
 }
 
