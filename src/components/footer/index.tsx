6dbe471b56ec2c74c4581f6a8e5a33b20ffcfe19
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-03 10:57:47 +0530

    Responsiveness added for footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <div className="pt-5 border-t border-[white] flex items-center justify-between text-sm font-normal pb-10">
+    <div className="pt-5 mt-3 border-t border-[white] flex flex-col md:flex-row  items-center justify-between text-sm font-normal pb-10">
       <p>@ 2024 Niraj Sathe. All rights reserved</p>
-      <div className="flex items-center gap-6 ">
+      <div className="flex items-center gap-6 md:mt-0 mt-2">
         <p>Terms of service</p>
         <p>Privacy policy</p>
         <p>Connect with me</p>
